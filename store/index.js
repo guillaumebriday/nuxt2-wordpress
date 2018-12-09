f0cf886d840a2aec956865ae6f9c8f6799bf873f
Guillaume Briday <guillaumebriday@gmail.com>
2018-12-10 00:00:10 +0100

    Adding store for posts

---
+++ store/index.js
@@ -0,0 +1,50 @@
+import Vuex from 'vuex'
+
+const store = () =>
+  new Vuex.Store({
+    state: {
+      post: null,
+      posts: []
+    },
+
+    mutations: {
+      setPost(state, data) {
+        state.post = data
+      },
+      setPosts(state, data) {
+        state.posts = data
+      }
+    },
+
+    actions: {
+      fetchPost({ commit }, params) {
+        return this.$axios
+          .get('/wp/v2/posts', {
+            params: {
+              slug: params.post
+            }
+          })
+          .then(({ data }) => {
+            commit('setPost', data[0])
+            return data[0]
+          })
+          .catch(error => {
+            return Promise.reject(error)
+          })
+      },
+
+      fetchPosts({ commit }) {
+        return this.$axios
+          .get('/wp/v2/posts')
+          .then(({ data }) => {
+            commit('setPosts', data)
+            return data
+          })
+          .catch(error => {
+            return Promise.reject(error)
+          })
+      }
+    }
+  })
+
+export default store
